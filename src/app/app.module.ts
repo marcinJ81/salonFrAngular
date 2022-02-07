b5a4fd39a937c5e61d3f8ed6bd7b31acfa95d6f6
Marcin Juranek <m.juranek@PGG.PL>
2022-02-07 11:41:08 +0100

    new version of project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClientListComponent } from './client/client-list/client-list.component';
+import { ClientComponent } from './client/client.component';
+import { ServicesComponent } from './services/services.component';
+import { ServicesListComponent } from './services/services-list/services-list.component';
+import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { ReservationListComponent } from './reservation/reservation-list/reservation-list.component';
+import { ReservationComponent } from './reservation/reservation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientListComponent,
+    ClientComponent,
+    ServicesComponent,
+    ServicesListComponent,
+    EmployeeListComponent,
+    EmployeeComponent,
+    ReservationListComponent,
+    ReservationComponent
   ],
   imports: [
     BrowserModule
